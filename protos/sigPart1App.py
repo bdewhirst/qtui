1920eda8e5f24d6a65d97b4f4f8cb68dc1e26479
Brian Dewhirst <b.dewhirst@gmail.com>
2024-05-23 12:59:16 -0400

    WIP-- electrical storm

--- protos/sigPart1App.py
+++ protos/sigPart1App.py
@@ -1,23 +1,14 @@
 import sys
-import random
-
 import PySide6.QtCore as psqt_core
 import PySide6.QtWidgets as psqt_widg
 
 
-window_titles = [
-    "An App",
-    "The App",
-    "Some App",
-    "This App",
-]
-
-# from a tutorial on signals, slots, and events. part 2.
+# from a tutorial on signals, slots, and events.
 class MainWindow(psqt_widg.QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.n_times_clicked = 0
+        self.button_is_checked = True
 
         self.setWindowTitle("Brian's App")
         self.setFixedSize(psqt_core.QSize(300, 400))  # opens to this size
@@ -25,19 +16,26 @@ class MainWindow(psqt_widg.QMainWindow):
         self.setMaximumSize(psqt_core.QSize(600, 900))  # specifies largest size
 
         self.button = psqt_widg.QPushButton("Please press here.")
+        self.button.setCheckable(True)
+        self.button.released.connect(
+            self.the_button_was_released
+        )  # this fires when you let go of the mouse button
         self.button.clicked.connect(self.the_button_was_clicked)
+        self.button.clicked.connect(self.the_button_was_toggled)
 
         self.setCentralWidget(self.button)
 
-    def the_button_was_clicked(self):
-        self.button.setText("This button has been clicked.")
-        new_window_title = random.choice(window_titles)
-        print("Setting title:  %s" % window_title)
-        if window_title == "Something went wrong":
-            self.button.setDisabled(True)
+    def the_button_was_released(self):
+        self.button_is_checked = self.button.isChecked()
+        print("release status:", self.button_is_checked)
 
-        # update window title:
-        self.setWindowTitle("A oneshot app based on a tutorial")
+    def the_button_was_clicked(self):
+        print("It was clicked.")
+
+    def the_button_was_toggled(self, checked):
+        # here's how you store state
+        self.button_is_checked = checked
+        print("check status:", self.button_is_checked)
 
 
 app = psqt_widg.QApplication(sys.argv)
