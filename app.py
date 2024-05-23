e8eb1d3efa73c4b77b06447b71e8536dc74b827c
Brian Dewhirst <b.dewhirst@gmail.com>
2024-05-23 13:33:07 -0400

    demo custom mouse events

--- app.py
+++ app.py
@@ -1,58 +1,32 @@
 import sys
-import random
 
-import PySide6.QtCore as psqt_core
 import PySide6.QtWidgets as psqt_widg
 
 
-window_titles = [
-    "My App",
-    "My App",
-    "Still My App",
-    "Still My App",
-    "What on earth",
-    "What on earth",
-    "This is surprising",
-    "This is surprising",
-    "Something went wrong",
-]
-
-# from a tutorial on signals, slots, and events. part 2.
+# from a tutorial on signals, slots, and events. part 4. (on events)
 # remember-- decouple / loosely couple; separate effects from what triggers them
 class MainWindow(psqt_widg.QMainWindow):
     def __init__(self):
         super().__init__()
+        self.label = psqt_widg.QLabel("Click in this window, please")
+        self.setCentralWidget(self.label)
 
-        self.n_times_clicked = 0
+    def mouseMoveEvent(self, e):
+        self.label.setText("mouseMoveEvent")
 
-        self.setWindowTitle("Brian's App")
-        self.setFixedSize(psqt_core.QSize(300, 400))  # opens to this size
-        self.setMinimumSize(psqt_core.QSize(250, 300))  # specifies smallest size
-        self.setMaximumSize(psqt_core.QSize(600, 900))  # specifies largest size
+    def mousePressEvent(self, e):
+        self.label.setText("mousePressEvent")
 
-        self.button = psqt_widg.QPushButton("Please press here.")
-        self.button.clicked.connect(self.the_button_was_clicked)
+    def mouseReleaseEvent(self, e):
+        self.label.setText("mouseReleaseEvent")
 
-        self.windowTitleChanged.connect(self.the_window_title_changed)
-
-        self.setCentralWidget(self.button)
-
-    def the_button_was_clicked(self):
-        print("This button has been clicked.")
-        new_window_title = random.choice(window_titles)
-        print("Setting title:  %s" % new_window_title)
-        # update window title:
-        self.setWindowTitle(new_window_title)
-
-    def the_window_title_changed(self, window_title):
-        print("Window title changed: %s" % window_title)
-        if window_title == "Something went wrong":  # note: 'something went wrong' is an element in that list
-            self.button.setDisabled(True)
+    def mouseDoubleClickEvent(self, e):
+        self.label.setText("mouseDoubleClickEvent")
 
 
 app = psqt_widg.QApplication(sys.argv)
 
 window = MainWindow()
-window.show()  # recall, this is invisible by default
+window.show()
 
-app.exec()  # main event loop
+app.exec()
